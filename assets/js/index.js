eeeba1dcb7c336a7b4cb1c645b8a64e7cd91534e
Harish Chouhan <me@harishchouhan.com>
2014-03-12 01:56:01 +0530

    Initial Commit

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+/**
+ * Main JS file for OST Ghost Theme's behaviours
+ */
+
+
+/*globals jQuery, document */
+(function ($) {
+		"use strict";
+
+		$(document).ready(function(){
+			$(".post-content").fitVids();
+		});
+
+}(jQuery));
